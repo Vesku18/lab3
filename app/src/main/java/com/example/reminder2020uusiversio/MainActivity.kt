806c9581ca02006cb430701b55b414bca6ce80b6
Vesku18 <vesa.simila@yahoo.com>
2020-03-13 00:19:37 +0200

    lab4 update on lab3

--- app/src/main/java/com/example/reminder2020uusiversio/MainActivity.kt
+++ app/src/main/java/com/example/reminder2020uusiversio/MainActivity.kt
@@ -3,28 +3,52 @@ package com.example.reminder2020uusiversio
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
+
 //import kotlinx.android.synthetic.main.list_view_item.view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main )
+        setContentView(R.layout.activity_main)
 
-        button_time.setOnClickListener{
-            val intent=Intent(applicationContext,TimeActivity::class.java)
+        button_time.setOnClickListener {
+            val intent = Intent(applicationContext, TimeActivity::class.java)
             startActivity(intent)
         }
 
-        button_map.setOnClickListener{
-            val intent=Intent(applicationContext,MapActivity::class.java)
+        button_map.setOnClickListener {
+            val intent = Intent(applicationContext, MapActivity::class.java)
             startActivity(intent)
         }
+    }
 
-        val data = arrayOf("Oulu","Helsinki","Tampere")
-        val reminderAdapter = ReminderAdapter(applicationContext, data)
-        list1.adapter = reminderAdapter
+    override fun onResume(){
+        super.onResume()
+        refreshList()
+    }
+
+    private fun refreshList(){
+        doAsync {
+            val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "reminders").build()
+            val reminders = db.reminderDao().getReminders()
+            db.close()
+
+            uiThread {
+                if (reminders.isNotEmpty()) {
+                    val adapter = ReminderAdapter(applicationContext, reminders )
+                    list1.adapter = adapter
+                } else {
+                    toast("No reminders")
+                }
+            }
+        }
+
     }
 
 }
--- app/src/main/java/com/example/reminder2020uusiversio/ReminderAdapter.kt
+++ app/src/main/java/com/example/reminder2020uusiversio/ReminderAdapter.kt
@@ -5,21 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_time.view.*
 import kotlinx.android.synthetic.main.list_view_item.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class ReminderAdapter(context: Context, private val list: Array<String>): BaseAdapter() {
+class ReminderAdapter(context: Context, private val list1: List<Reminder>): BaseAdapter() {
 
     private val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
         val row = inflater.inflate(R.layout.list_view_item, parent, false)
-        row.itemMessage.text = list[position]
-        row.itemTrigger.text = "hello"
+        row.itemMessage.text = list1[position].message
+
+        if(list1[position].time != null){
+            val sdf = SimpleDateFormat("HH:mm dd.MM.yyyy")
+            sdf.timeZone = TimeZone.getDefault()
+
+            val time = list1[position].time
+            val readableTime =sdf.format(time)
+
+            row.itemTrigger.text = readableTime
+        }
+        else{
+            row.itemTrigger.text = "location"
+        }
+
+        //row.itemTrigger.text = list1[position].time.toString()
         return row
     }
 
     override fun getItem(position: Int): Any {
-        return list[position]
+        return list1[position]
     }
 
     override fun getItemId(position: Int): Long {
@@ -27,7 +45,7 @@ class ReminderAdapter(context: Context, private val list: Array<String>): BaseAd
     }
 
     override fun getCount(): Int {
-        return list.size
+        return list1.size
     }
 }
 
